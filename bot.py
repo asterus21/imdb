fad1e4af3c30f0228ceb190056fd67b5eceeb5ae
Mike <70848378+asterus21@users.noreply.github.com>
2021-03-03 14:15:52 +0300

    remove the key

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ def to_watch():
 	text = films_list[random.randint(0, len(films_list) + 1)]
 	return text
 
-bot = telebot.TeleBot('here is your API key)
+bot = telebot.TeleBot('here is your API key')
 
 @bot.message_handler(commands=['start'])
 def send_weclome(message):
